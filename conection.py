6566c0e2d0db21047a7caf6308b0846cb6404046
Estuardo07 <53918904+Estuardo07@users.noreply.github.com>
2020-05-30 21:04:37 -0600

    Add files via upload

--- conection.py
+++ conection.py
@@ -1,10 +1,8 @@
 from py2neo import Graph, NodeMatcher
 
-grafo = Graph("bolt://localhost:7687", auth=("neo4j","1234"), encrypted = False)
+grafo = Graph("bolt://localhost:7687", auth=("neo4j","1234"))
 matcher = NodeMatcher(grafo)
 
-#7474
-#7473
 
 def menu():
     
@@ -13,21 +11,25 @@ def menu():
     print ("            RECOMENDACION DE LUGARES TURÍSTICOS  \n")
     
     print (" -- ACTIVIDADES: \n")
-    print ("   1. Aventuras ")
-    print ("   2. Deportes Extremos ")
-    print ("   3. Recreacion \n")
-    actividad = input (" - De las opciones anteriores, escoja la que más se adecue a su gusto (escriba la opcion tal y como aparece): ")
+    print ("   - Aventuras ")
+    print ("   - Deportes Extremos ")
+    print ("   - Recreacion \n")
+    actividad = input (" - De las opciones anteriores, escoja la que más se adecue a su gusto (escriba la opcion tal y como aparece, sin '-'): ")
 
 
     print (" -- CLIMAS: \n")
-    print ("   1. Frio")
-    print ("   2. Calido ")
-    print ("   3. Templado \n")
-    clima = input (" - De las opciones anteriores, escoja la que más se adecue a su gusto (escriba la opcion tal y como aparece): ")
+    print ("   - Frio")
+    print ("   - Calido ")
+    print ("   - Templado \n")
+    clima = input (" - De las opciones anteriores, escoja la que más se adecue a su gusto (escriba la opcion tal y como aparece, sin '-'): ")
 
 
     print (" -- NÚMERO DE PERSONAS: \n")
-    personas = input (" - Ingrese el numero de personas que lo acompañarian (incluyendo a ud):  ")
+    print ("   - Grupos")
+    print ("   - Parejas ")
+    print ("   - Solo ")
+    print ("   - Familia \n")
+    personas = input (" - De las opciones anteriores, escoja la que más se adecue a su gusto (escriba la opcion tal y como aparece, sin '-'): ")
 
 
 #    statement1 = "MATCH(I:Lugar) <-[:pertenece]-(s:Actividades{Actividades: '" +actividad+ "'}) return l"
@@ -44,7 +46,7 @@ def menu():
 
     matcher.match("lugar", Actividades="Aventuras").first()
     matcher.match("lugar", Clima="Frio").first()
-    matcher.match("lugar", Personas="1").first()
+    matcher.match("lugar", Personas="Solo").first()
     
     print(actividad)
     print(clima)
